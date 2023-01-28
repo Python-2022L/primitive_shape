634a8eef91fcd0308f1f547b69a940aefd20ca47
NurbekSaitqulov03 <nurbek.saitqulov23@gmail.com>
2023-01-28 22:07:28 +0500

    polygon.py

---
+++ polygon.py
@@ -0,0 +1,8 @@
+class Polygon:
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+    def getArea(self):
+        pass
+    def getPerimeter(self):
+        pass
