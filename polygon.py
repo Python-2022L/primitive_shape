3607c98d67649bf57e7ce3e45e3d0b57b8315d19
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2023-01-28 12:55:50 +0500

    perimetr va yuzi

---
+++ polygon.py
@@ -0,0 +1,10 @@
+class Polygon:
+    def __init__(self, height ,width):
+        self.height = height
+        self.width = width
+    def getArea (self):
+        return self.height*self.width
+    def getPerimeter(self):
+        return 2*(self.height+self.width)
+aaaa = Polygon(2,3)
+print(aaaa.getPerimeter())
