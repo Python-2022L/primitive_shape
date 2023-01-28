ee1dbcab94d5bfec19ce17fcccab1916ae88e565
ilhom <ilhommamatqulov276@gmail.com>
2023-01-28 12:39:58 +0500

    rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -2,3 +2,6 @@ from polygon import Polygon
 class Rectangle(Polygon):
     def __init__(self, height, width):
         super().__init__(height, width)
+rect = Rectangle(3, 4)
+print(rect.getArea()) # 12
+print(rect.getPerimeter()) # 14
